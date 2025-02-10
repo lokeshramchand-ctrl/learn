9b0bca778b44ac7f3cb0834fc8b7511b4027017e
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2025-02-11 01:00:21 +0530

    1.00

--- learn/urls.py
+++ learn/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    
 ]
     
 
